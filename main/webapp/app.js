398e8cbecff465415e6f2677ed71dd8a9a7ba574
maurodanielpenaloza <maurodanielpenaloza@gmail.com>
2016-06-26 14:43:46 -0300

    Primer Commit de la aplicacion

---
+++ main/webapp/app.js
@@ -0,0 +1,45 @@
+var app=angular.module('app', ['ui.router', 'ngStorage', 'ui.bootstrap', 'ngTouch'])
+
+.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+	
+	$urlRouterProvider.otherwise('/welcome');
+	$stateProvider
+	.state('hello',{
+		abstract: true,
+		views: {
+			'':{
+				templateUrl: 'views/hello.html'	
+			}
+		}
+	})
+	.state('hello.welcome',{
+		url: '/welcome',
+		templateUrl: 'views/welcome.html'
+	})
+	.state('hello.login',
+	{
+		url: '/login',
+		templateUrl: 'views/login.html'
+	})
+	.state('app', {
+		abstract: true,
+		views:{
+			'':{
+				templateUrl: 'views/layout.html'
+			}
+			
+		}
+	})
+	.state('app.home',{
+		url:'/home',
+		templateUrl:'views/home.html',
+		controller: 'homeController'
+	})
+	.state('app.reporte',{
+		url:'/reporte',
+		templateUrl:'views/reporte.html',
+	});
+	
+
+
+}]);
